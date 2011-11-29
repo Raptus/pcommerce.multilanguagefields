35f1909159227dac3b5d250b1cad7289410abf01
Simon Käser <skaeser@raptus.com>
2011-11-29 14:34:01 +0000

    Removed reindexing from setuphandler

--- pcommerce/multilanguagefields/setuphandlers.py
+++ pcommerce/multilanguagefields/setuphandlers.py
@@ -5,13 +5,6 @@ from Products.CMFCore.utils import getToolByName
 extenders = [extender.ProductExtender,
              extender.VariationExtender]
 
-indexes = ('SearchableText', 'Subject', 'Title', 'Description', 'sortable_title',)
-
-def reindex(portal):
-    catalog = getToolByName(portal, 'portal_catalog')
-    for index in indexes:
-        catalog.reindexIndex(index, portal.REQUEST)
-
 def install(context):
     if context.readDataFile('pcommerce.multilanguagefields_install.txt') is None:
         return
@@ -20,8 +13,6 @@ def install(context):
     sm = portal.getSiteManager()
     for extender in extenders:
         sm.registerAdapter(extender, name='MultilanguagePcommerce%s' % extender.__name__)
-    
-    reindex(portal)
 
 def uninstall(context):
     if context.readDataFile('pcommerce.multilanguagefields_install.txt') is None and \
@@ -32,5 +23,3 @@ def uninstall(context):
     sm = portal.getSiteManager()
     for extender in extenders:
         sm.unregisterAdapter(extender, name='MultilanguagePcommerce%s' % extender.__name__)
-    
-    reindex(portal)
